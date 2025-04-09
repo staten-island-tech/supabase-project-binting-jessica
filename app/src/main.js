4f4aaa5e06081c4b55ab2aed3b6c40090cab859f
JessL868 <144480854+JessL868@users.noreply.github.com>
2025-04-09 14:02:41 -0400

    installed

--- app/src/main.js
+++ app/src/main.js
@@ -5,10 +5,16 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { createClient } from '@supabase/supabase-js'
 
+const supabase = createClient('https://lbcithrzijletyauhsnm.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxiY2l0aHJ6aWpsZXR5YXVoc25tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMwMTAxNzMsImV4cCI6MjA1ODU4NjE3M30.7_I-Na4UBJcclN-_KmaA9KZLiKAA1rk6O-ewQjF0pMk')
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+const { data, error } = await supabase
+  .from('busers')
+  .select()
