64fb190c5ab839cb7d4f7c167dc2321c884d7fdd
JessL868 <144480854+JessL868@users.noreply.github.com>
2025-04-10 14:02:42 -0400

    ad new key and link

--- app/src/main.js
+++ app/src/main.js
@@ -7,7 +7,7 @@ import App from './App.vue'
 import router from './router'
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient('https://lbcithrzijletyauhsnm.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxiY2l0aHJ6aWpsZXR5YXVoc25tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMwMTAxNzMsImV4cCI6MjA1ODU4NjE3M30.7_I-Na4UBJcclN-_KmaA9KZLiKAA1rk6O-ewQjF0pMk')
+const supabase = createClient('', '')
 const app = createApp(App)
 
 app.use(createPinia())
