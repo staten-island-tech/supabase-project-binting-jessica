92d5f05eab972219fc245cf2c834907fd5c87dea
JessL868 <144480854+JessL868@users.noreply.github.com>
2025-06-03 13:59:28 -0400

    create funcitons for variables tmr

--- app/src/stores/posts.js
+++ app/src/stores/posts.js
@@ -1,13 +1,23 @@
 import { defineStore } from 'pinia'
 import { supabase } from '../lib/supabaseClient'
 
+const props = defineProps({
+  user: Object,
+  auth: Object
+})
+
 export const useAuthStore = defineStore('profile', {
   state: () => ({
-    followers: 0,
-    cooking_experience: null,
-    posts: 0
+    title: null,
+    images: [],
+    likes: 0,
+    // timestamp: ,
+    content: null,
+    id: auth.user.id,
   }),
   actions: {
-    
+    likes() {
+      this.likes++
+    }
   }
 })
