847407514179e6f2616f01704efbb1aaa1f95054
binting1 <bintingchenn123@gmail.com>
2025-06-13 03:02:22 -0400

    main done

--- app/src/stores/counter.js
+++ app/src/stores/counter.js
@@ -3,29 +3,36 @@ import { supabase } from '../lib/supabaseClient'
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
-    user: null
+    user: null,
+    cookingexperience: 1,
   }),
   actions: {
     async signUp(username, email, password) {
-      const { data, error } = await supabase.auth.signUp({ username, email, password })
+      const { data, error } = await supabase.auth.signUp({email, password,
+        options: {
+          data: { username }
+        }
+       })
       if (error) throw error
       this.user = data.user
     },
 
-    async signIn(username, email, password) {
-      const { data, error } = await supabase.auth.signInWithPassword({ username, email, password })
+    async signIn(email, password) {
+      const { data, error } = await supabase.auth.signInWithPassword({email, password,})
       if (error) throw error
       this.user = data.user
     },
 
     async signOut() {
-      await supabase.auth.signOut()
+      const { error } = await supabase.auth.signOut();
+      if (error) throw error;
       this.user = null
     },
     async fetchUser() {
-      const { data, error } = await supabase.auth.getUser()
-      if (error) throw error
-      this.user = data?.user ?? null
-    }
+      const user = await supabase.auth.user;
+      if (user) {
+        this.user = data.user;
+      }
+    },
   }
 })
--- app/src/views/ProfilePage.vue
+++ app/src/views/ProfilePage.vue
@@ -1,11 +1,39 @@
 <template>
-    <div>
+    <div v-if="auth.user" >
+        <h1> welcome, {{ auth.user.email }}</h1>
+        <p>Cooking Experience: {{ auth.cookingexperience}} </p>
+        <button class="out" @click="handleSignOut">log out here</button>
     </div>
+    <div  v-else>Not logged in</div>
 </template>
 
 <script setup>
+import router from '@/router';
+import { useAuthStore } from '../stores/counter.js'
+import { ref, onMounted } from 'vue';
+import { supabase } from '../lib/supabaseClient.js';
+
+const auth = useAuthStore () 
+const cookingexperience = ref("");
+
+onMounted (async () => {
+    await auth.fetchUser();
+    console.log(auth.user);
+})
+
+const handleSignOut = async () => {
+    try {
+        await auth.signOut()
+        alert('Logged out successfully');
+        auth.user = null
+        router.push('/')
+    } catch (err) {
+        console.error('Failed to log out:', err.message)
+        alert('failed') 
+    }
+}
 </script>
 
-<style  scoped>
+<style lang="scss" scoped>
 
 </style>
